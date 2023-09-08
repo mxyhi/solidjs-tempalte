eba4e37ae718ff81d7826ac00ce820c1fec5489a
mxyhi <2528988582@qq.com>
2023-09-08 08:58:10 +0800

    🎉

--- vite.config.ts
+++ vite.config.ts
@@ -30,6 +30,7 @@ export default defineConfig({
             display: 'inline-block',
             'vertical-align': 'middle',
           },
+          autoInstall: true,
         }),
       ],
     }),
